307e0129474067bd21180a3396f449cd4fbbf635
LesterCantor <c012718j@student.staffs.ac.uk>
2024-11-11 19:34:48 +0000

    comments in code and simple inverted controls

--- player.gd
+++ player.gd
@@ -4,13 +4,22 @@ class_name Player
 @export var SPEED: float = 5.0
 @export var ACCELERATION: float = 4.0
 
+# Check if the player is in the mirror dimension
+var should_invert: bool = false
+
+var input_dir: Vector2
 
 func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("left", "right", "down", "up")
-	var direction := (transform.basis * Vector3(input_dir.x, input_dir.y, 0)).normalized()
+	input_dir = Input.get_vector("left", "right", "down", "up")
+	var direction: Vector3 = (transform.basis * Vector3(input_dir.x, input_dir.y, 0)).normalized()
 	
+	# Swaps the player's controls
+	if should_invert:
+		direction = -direction
+	
+	# Moves the player depending on the input they are pressing, with acceleration
 	if input_dir.length() > 0:
 		velocity = velocity.move_toward(direction * SPEED, delta * ACCELERATION)
 	else:
